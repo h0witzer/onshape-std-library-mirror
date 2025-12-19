4a278dc75091ce053ccd66563f7e9d1d906201c2
Derek Van Allen <36012268+h0witzer@users.noreply.github.com>
2025-12-19 14:59:29 -0500

    Update FeatureScript and import paths in textFunctionsPascoe.fs
    Updated FeatureScript version and imports in textFunctionsPascoe.fs.

--- custom-features/textFunctionsPascoe.fs
+++ custom-features/textFunctionsPascoe.fs
@@ -6,16 +6,13 @@
 //_______________________________________________________________________________________________________________________________________________
 
 
-FeatureScript 2260;
-import(path : "onshape/std/common.fs", version : "2260.0");
+FeatureScript 2837;
+import(path : "onshape/std/common.fs", version : "2837.0");
+import(path : "onshape/std/projectiontype.gen.fs", version : "2837.0");
 
 // CADSharp
 export import(path : "cbeb3dcf671e00785597bd76/409d65a3744fe434f32bdffc/a75ab01def146a42f55baa7f", version : "381046010d5aea697e433948");
-
-// export import(path : "c7c08274a0d273b9a5f5b47d/16aa9873224c4a0cc2412bfd/6b9d1a8ad65ac54fe2125581", version : "b1c9eb4800a01f8a29c6c12a");
-// export import(path : "c7c08274a0d273b9a5f5b47d/16aa9873224c4a0cc2412bfd/5859f548d83656e6f7c0d427", version : "5474fc5269dcc948135c4857");
-import(path : "c7c08274a0d273b9a5f5b47d/f89b4047602171084b43cd13/af743c73c677355164494e79", version : "41b9668ee979c27b9ac0cb92");
-export import(path : "12312312345abcabcabcdeff/8e6d551b2d41cf932b1681f6/ec739638f0a117a475a744af", version : "312fee4ee687399466575a10");
+import(path : "c7c08274a0d273b9a5f5b47d/f89b4047602171084b43cd13/af743c73c677355164494e79", version : "41b9668ee979c27b9ac0cb92");//functionsPascoe.fs
 
 icon::import(path : "4cba10e99d49d95b30708864", version : "7882b03d25a2320567ce70df");
 
