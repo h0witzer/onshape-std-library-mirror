09488d1b690fe42fb9aa82973df86c407f3ed7b1
Derek Van Allen <36012268+h0witzer@users.noreply.github.com>
2025-12-19 14:53:46 -0500

    Update import statements in textPascoe.fs

--- custom-features/textPascoe.fs
+++ custom-features/textPascoe.fs
@@ -14,10 +14,10 @@ export import(path : "cbeb3dcf671e00785597bd76/409d65a3744fe434f32bdffc/a75ab01d
 
 // export import(path : "c7c08274a0d273b9a5f5b47d/16aa9873224c4a0cc2412bfd/6b9d1a8ad65ac54fe2125581", version : "b1c9eb4800a01f8a29c6c12a");
 // export import(path : "c7c08274a0d273b9a5f5b47d/16aa9873224c4a0cc2412bfd/5859f548d83656e6f7c0d427", version : "5474fc5269dcc948135c4857");
-import(path : "c7c08274a0d273b9a5f5b47d/f89b4047602171084b43cd13/af743c73c677355164494e79", version : "41b9668ee979c27b9ac0cb92");
+import(path : "c7c08274a0d273b9a5f5b47d/f89b4047602171084b43cd13/af743c73c677355164494e79", version : "41b9668ee979c27b9ac0cb92");//functionsPascoe.fs
 export import(path : "12312312345abcabcabcdeff/8e6d551b2d41cf932b1681f6/ec739638f0a117a475a744af", version : "312fee4ee687399466575a10");
-export import(path : "b2fd066143872bf1cd73ced7", version : "21c1652f0a3e50a5aa263d9f");
-icon::import(path : "8842c738c4b2ed64b2c83a97", version : "2370ac0bd968616284b5a4ef");
+export import(path : "5d523046fa535976e27cb329", version : "a0d2d211a3e891400f53d991");//textFunctionsPascoe.fs
+icon::import(path : "4cba10e99d49d95b30708864", version : "7882b03d25a2320567ce70df");
 
 annotation {
         "Feature Type Name" : "Text",
