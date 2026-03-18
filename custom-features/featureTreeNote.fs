16ce607a59e76632c845a990d0f754c41efdd5ff
Derek Van Allen <36012268+h0witzer@users.noreply.github.com>
2026-03-18 15:55:41 -0400

    Enhance featureTreeNote with additional annotations
    Updated feature type description and annotations for clarity.

--- custom-features/featureTreeNote.fs
+++ custom-features/featureTreeNote.fs
@@ -1,5 +1,7 @@
 FeatureScript 2796;
 import(path : "onshape/std/feature.fs", version : "2796.0");
+import(path : "onshape/std/featureList.fs", version : "2796.0");
+import(path : "onshape/std/debug.fs", version : "2796.0");
 ICON::import(path : "5db36f2dbd94dae497f2c12f", version : "e12a2e27652d4be6e377c94f");
 
 annotation { "Feature Type Name" : "Feature tree note",
@@ -7,7 +9,7 @@ annotation { "Feature Type Name" : "Feature tree note",
             "UIHint" : UIHint.NO_PREVIEW_PROVIDED,
             "Tooltip Template" : "#subject: #description",
             "Icon" : ICON::BLOB_DATA,
-            "Feature Type Description" : "This feature does nothing, it just allows you to store and show some notes in the feature tree, like a READ ME note" }
+            "Feature Type Description" : "This feature does nothing to the context or model, it just allows you to store and show some notes in the feature tree, like a READ ME note, and highlight referenced geometry" }
             
 export const featureTreeNote = defineFeature(function(context is Context, id is Id, definition is map)
     precondition
@@ -15,14 +17,25 @@ export const featureTreeNote = defineFeature(function(context is Context, id is 
         annotation { "Name" : "Subject", "UIHint" : UIHint.VARIABLE_NAME, "MaxLength" : 256 }
         definition.subject is string;
 
-        annotation { "Name" : "Description (shows on hover)", "MaxLength" : 512, "Default" : ""  }
+        annotation { "Name" : "Description (shows on hover)", "MaxLength" : 512 }
         definition.description is string;
         
-        annotation { "Name" : "More text here" }
+        annotation { "Name" : "More text here", "MaxLength" : 640000  }
         definition.text is string;
-
+        
+        annotation { "Name" : "Reference geometry", "Filter" : AllowMeshGeometry.YES && AllowFlattenedGeometry.YES, 
+        "UIHint" : UIHint.PREVENT_CREATING_NEW_MATE_CONNECTORS }
+        definition.geometry is Query;
+        
+        annotation { "Name" : "Reference features", "UIHint" : UIHint.ALLOW_FLAT_SKETCH_SELECTION }
+        definition.features is FeatureList;
     }
-    {
-        const subject = definition.subject;
-        const description  = definition.description;
+    // body: 
+    {    
+        try silent
+            {
+                addDebugEntities(context, definition.geometry, DebugColor.YELLOW);
+            
+                setHighlightedEntities(context, { "entities": definition.geometry });
+            }
     });
